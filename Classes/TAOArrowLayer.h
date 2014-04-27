2e536e40bd3729481d980338bf81a22a6f64999c
Or Sharir <or@sharir.org>
2014-04-27 15:57:22 +0300

    Add documentation
    Signed-off-by: Or Sharir <or@sharir.org>

--- Classes/TAOArrowLayer.h
+++ Classes/TAOArrowLayer.h
@@ -1,18 +1,33 @@
-//
-//  TAOArrowLayer.h
-//  Here a Story
-//
-//  Created by Or Sharir on 11/28/13.
-//  Copyright (c) 2013 TAO Project. All rights reserved.
-//
+//  Created by Or Sharir on 11/14/13.
+//  Copyright (c) 2013 Three Tall Guys Ltd. All rights reserved.
 
 #import <QuartzCore/QuartzCore.h>
-
+/**
+ *  A layer showing a curve that ends with an arrow.
+ */
 @interface TAOArrowLayer : CALayer
+/**
+ *  The starting point of the curve.
+ */
 @property (assign, nonatomic) CGPoint start;
+/**
+ *  The ending point of the curve where the arrow head is at.
+ */
 @property (assign, nonatomic) CGPoint end;
+/**
+ *  The control point of the curve. The curve passes near the point.
+ */
 @property (assign, nonatomic) CGPoint by;
+/**
+ *  The width of the line.
+ */
 @property (assign, nonatomic) CGFloat lineWidth;
+/**
+ *  The color of the line.
+ */
 @property (strong, nonatomic) UIColor* strokeColor;
+/**
+ *  Updates the visible state of the layer.
+ */
 - (void)updateDisplay;
 @end
--- Classes/TAOOverlayHelp.h
+++ Classes/TAOOverlayHelp.h
@@ -1,16 +1,45 @@
-//
-//  TAOOverlayHUD.h
-//  Here a Story
-//
 //  Created by Or Sharir on 11/14/13.
-//  Copyright (c) 2013 TAO Project. All rights reserved.
-//
+//  Copyright (c) 2013 Three Tall Guys Ltd. All rights reserved.
 
 #import <UIKit/UIKit.h>
+/**
+ *  Simple block callback.
+ */
 typedef void (^TAOOverlayHelpCompletionBlock)(void);
+/**
+ *  Overlay HUD for showing help tips to controls on screen.
+ *
+ *  The control shows a semi-transparent overlay with a message and an arrow going from the message to a point on screen.
+ *  The control can be dismissed by code, or by the user tapping the overlay.
+ */
 @interface TAOOverlayHelp : UIView
+/**
+ *  Shows the hud with the given status message and pointing to the given point.
+ *
+ *  It's possible to call this method multiple times before the user dismisses the overlay, forming a stack of messages,
+ *  showing the next message after dismissing the last one.
+ *
+ *  @param status          The message to show to the user.
+ *  @param point           The point on screen to which point the arrow.
+ *  @param didDismissBlock A call back after the user (or by code) dismisses the overlay.
+ *
+ *  @return  A key identifying the message. Can be used to dismiss a specific message from the stack.
+ */
 + (NSString*)showWithHelpTip:(NSString*)status pointAt:(CGPoint)point didDismiss:(TAOOverlayHelpCompletionBlock)didDismissBlock;
+/**
+ *  Dismiss all visible messages.
+ */
 + (void)dismiss;
+/**
+ *  Dismiss a specific message using its key returned when creating the message.
+ *
+ *  @param key The key of the message.
+ */
 + (void)dismiss:(NSString*)key;
+/**
+ *  Returns if the overlay view is currently visible.
+ *
+ *  @return YES if the overlay view is visible. NO overwise.
+ */
 + (BOOL)isVisible;
 @end
